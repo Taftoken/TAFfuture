195d76ba34748b0634f012db960909d82092f50d
bornbig <72680443+bornbig@users.noreply.github.com>
2022-03-04 22:36:38 +0530

    Updated Vesting contract

--- contracts/TAF2MonthsVesting.sol
+++ contracts/TAF2MonthsVesting.sol
@@ -3,14 +3,19 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "./libs/DateTime.sol";
 
 contract TAFTokenVesting{
 
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
+    using BokkyPooBahsDateTimeLibrary for uint;
 
     // ERC20 basic token contract being held
     IERC20 private immutable _token;
+
+    // timestamp when token release is enabled
+    uint256 private _releaseDate;
 
     // beneficiary of tokens after they are released
     address private immutable _beneficiary;
@@ -19,6 +24,7 @@ contract TAFTokenVesting{
     constructor(address token_) {
         _token = IERC20(token_);
         _beneficiary = msg.sender;
+        _releaseDate = BokkyPooBahsDateTimeLibrary.addMonths(block.timestamp, 2);
     }
 
     /**
@@ -35,11 +41,19 @@ contract TAFTokenVesting{
         return _beneficiary;
     }
 
+    /**
+     * @return the time when the tokens are released.
+     */
+    function releaseTime() public view virtual returns (uint256) {
+        return _releaseDate;
+    }
+
      
     /**
      * @notice Transfers tokens held by timelock to beneficiary.
      */
     function release() public virtual {
+        require(block.timestamp >= _releaseDate, "TokenTimelock: current time is before release time");
         require(msg.sender == _beneficiary, "You can not use this function");
         
         uint256 amount = token().balanceOf(address(this));
