25160098bdefcc82c465daf98438f836dda66bde
bornbig <72680443+bornbig@users.noreply.github.com>
2022-04-07 21:35:01 +0530

    updated taf 21 contract

--- contracts/stakings/fixedAPY/TAF21DaysStaking.sol
+++ contracts/stakings/fixedAPY/TAF21DaysStaking.sol
@@ -93,9 +93,12 @@ contract TAF21DaysStaking {
     function stake(uint _amount) public{
         require(!paused, "Staking contract is paused!");
         require(block.timestamp < poolExpiry, "Staking contract is over!");
-        require(_amount >= minUserStakingAmount, "Must be above min user staking amount");
 
-        require(balances[msg.sender] + _amount <= maxUserStakingAmount, "Must be less then max user staking amount");
+        if(minUserStakingAmount > 0)
+            require(_amount >= minUserStakingAmount, "Must be above min user staking amount");
+
+        if(maxUserStakingAmount > 0)
+            require(balances[msg.sender] + _amount <= maxUserStakingAmount, "Must be less then max user staking amount");
 
         if(maxTotalStakingAmount > 0)
             require(totalSupply + _amount <= maxTotalStakingAmount, "Max Staking amount reached");
@@ -163,7 +166,12 @@ contract TAF21DaysStaking {
     function withdrawReward() public{
         uint256 amount = earned();
 
-        rewardsToken.transfer(msg.sender, amount);
+        uint256 fee = (withdrawFee * amount) / 10000;
+
+        rewardsToken.transfer(msg.sender, amount - fee);
+
+        if(fee > 0)
+            rewardsToken.transfer(owner, fee);
 
         rewardsOut[msg.sender] += amount;
     }
@@ -178,11 +186,15 @@ contract TAF21DaysStaking {
     }
 
     function timings() public view returns (uint256, uint256){
-        return (timestamp[msg.sender], timestamp[msg.sender] + 28 days);
+        return (timestamp[msg.sender], poolExpiry + 7 days);
     }
 
     function userInfo() public view returns (uint256, uint256, uint256, uint256){
         (uint256 start, uint256 end) = timings();
         return (balances[msg.sender], earned(), start, end);
+    }
+
+    function isForceUnstakeNeeded() public view returns(bool){
+        return block.timestamp < poolExpiry + 7 days;
     }
 }
