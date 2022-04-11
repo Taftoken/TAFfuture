cbd008734f329f8fda5a4d3189a627d6247e7dc4
bornbig <72680443+bornbig@users.noreply.github.com>
2022-04-11 17:19:51 +0530

    Updated only owner as modifier

--- contracts/payment-gateway/TAFInvoiceFactory.sol
+++ contracts/payment-gateway/TAFInvoiceFactory.sol
@@ -8,16 +8,21 @@ contract TAFInvoiceFactory{
 
     address immutable public owner;
     mapping(address => bool) public isAdmin;
-    mapping(uint256 => uint256) public amountForOrder;
-    mapping(uint256 => address) public tokenForOrder;
-    mapping(uint256 => bool) public isOrderPaid;
+    mapping(string => uint256) public amountForOrder;
+    mapping(string => address) public tokenForOrder;
+    mapping(string => bool) public isOrderPaid;
+
+    modifier onlyOwner {
+      require(msg.sender == owner);
+      _;
+   }
 
     constructor(){
         owner = msg.sender;
         isAdmin[msg.sender] = true;
     }
 
-    function createNewInvoice(address token, uint256 amount, uint256 order_id) public{
+    function createNewInvoice(address token, uint256 amount, string memory order_id) public{
         require(isAdmin[msg.sender], "Only admin can create an Invoice");
         require(!isOrderPaid[order_id], "The order is already paid");
         amountForOrder[order_id] = amount;
@@ -25,25 +30,22 @@ contract TAFInvoiceFactory{
     }
 
 
-    function withdraw(uint256 amount, address reciver, address token) public{
-        require(msg.sender == owner, "Only Owner Can Withdraw Tokens");
+    function withdraw(uint256 amount, address reciver, address token) onlyOwner public{
         require(IERC20(token).balanceOf(address(this)) >= amount, "Amount is more then balance");
         IERC20(token).transfer(reciver, amount);
     }
 
-    function pay(uint256 order_id) public{
+    function pay(string memory order_id) public{
         require(!isOrderPaid[order_id], "The order is already paid");
         require(IERC20(tokenForOrder[order_id]).transferFrom(msg.sender, address(this), amountForOrder[order_id]));
         isOrderPaid[order_id] = true;
     }
 
-    function addNewAdmin(address user) public{
-        require(msg.sender == owner, "Only Owner Can Add new admin");
+    function addNewAdmin(address user) onlyOwner public{
         isAdmin[user] = true;
     }
 
-    function removeAdmin(address user) public{
-        require(msg.sender == owner, "Only Owner Can Add new admin");
+    function removeAdmin(address user) onlyOwner public{
         isAdmin[user] = false;
     }
 
